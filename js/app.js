f7720e4e04fbf647c8d68c8ee97d155269fe6fdc
Himanshu Soni <blaringblazehs@gmail.com>
2019-01-22 23:23:50 +0530

    First commit
    first website commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+  var config = {
+    apiKey: "AIzaSyCU7rLDrX8XoMxiMfeX9WJ58bP8-LPRN3U",
+    authDomain: "aloysius-photos.firebaseapp.com",
+    databaseURL: "https://aloysius-photos.firebaseio.com",
+    projectId: "aloysius-photos",
+    storageBucket: "aloysius-photos.appspot.com",
+    messagingSenderId: "81724430929"
+  };
+  firebase.initializeApp(config);
+
+firebase.auth.Auth.Persistence.LOCAL; 
+
+    $("#btn-login").click(function(){
+        
+        var email = $("#email").val();
+        var password = $("#password").val(); 
+
+        var result = firebase.auth().signInWithEmailAndPassword(email, password);
+    
+        result.catch(function(error){
+            var errorCode = error.code; 
+            var errorMessage = error.message; 
+
+            console.log(errorCode);
+            console.log(errorMessage);
+        });
+
+    });
+
+   function switchView(view){
+        $.get({
+            url:view,
+            cache: false,  
+        }).then(function(data){
+            $("#container").html(data);
+        });
+    }
